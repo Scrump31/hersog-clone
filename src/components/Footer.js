98b62e4e6a46efed520ac1e1ae653319ba7f9078
Scottie Crump <scottiecrump@yahoo.com>
2017-06-13 00:08:08 -0400

    styled footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,23 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="container">
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div className="footer-logo-wrapper">
+        <div className="logo-border-top">
+          <div className="logo">HERSOG</div>
+        </div>
+      </div>
+
+      <div>
+        <ul className="footer-nav">
+          <li><a href="">ABOUT US</a></li>
+          <li><a href="">PROJECTS</a></li>
+          <li><a href="">SERVICES</a></li>
+          <li><a href="">PUBLICATIONS</a></li>
+          <li><a href="">CAREERS</a></li>
+        </ul>
+      </div>
+
+    </footer>
   );
 }
